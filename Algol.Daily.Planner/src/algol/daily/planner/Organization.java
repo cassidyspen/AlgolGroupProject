11c68820c194a7a207da822f96ce466db69d578d
Michael Gallagher <michaelgallagher@sandiego.edu>
2022-11-08 17:24:07 -0800

    Organization file

---
+++ Algol.Daily.Planner/src/algol/daily/planner/Organization.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package algol.daily.planner;
+import java.util.ArrayList; 
+
+/**
+ * Organization object represents a list of Assignments
+ * 
+ * @author mikey
+ */
+public class Organization {
+    private ArrayList<Assignment> assignmentList = new ArrayList<>();
+    
+    /**
+     * sorts by urgency
+     */
+    public void urgencySort() {
+        
+    }
+    
+    /**
+     * sorts by due date
+     */
+    public void daySort() {
+        
+    }
+    
+    /**
+     * sorts by progress
+     */
+    public void progressSort() {
+        
+    }
+    
+    /**
+     * displays assignmentList
+     */
+    public void display(){
+        
+    }
+}
