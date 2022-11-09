c1d3c1ea98e73fd869bf3d898295c01d421c0042
Michael Gallagher <michaelgallagher@sandiego.edu>
2022-11-08 17:23:26 -0800

    homework file

---
+++ Algol.Daily.Planner/src/algol/daily/planner/Homework.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package algol.daily.planner;
+
+/**
+ * A subclass of Assignment representing a Homework assignment
+ * 
+ * @author mikey
+ */
+public class Homework extends Assignment {
+    public Homework() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
